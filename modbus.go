af636e9b53b528654737fc7b6e95a9aef06c1481
Evert Jan Hakvoort <evertjan03@gmail.com>
2024-11-06 22:01:35 +0100

    fix: panic when transport is not initialized

--- modbus.go
+++ modbus.go
@@ -70,6 +70,7 @@ const (
 	ErrBadTransactionId          Error = "bad transaction id"
 	ErrUnknownProtocolId         Error = "unknown protocol identifier"
 	ErrUnexpectedParameters      Error = "unexpected parameters"
+	ErrTransportNotInitialized   Error = "Transport is not initialized"
 )
 
 // mapExceptionCodeToError turns a modbus exception code into a higher level Error object.
